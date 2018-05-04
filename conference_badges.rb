ef7e0302294b1c972a42833001800b77fc920e7c
malorie16 <malorie18@gmail.com>
2018-05-04 13:21:23 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,6 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator([attendees])
-  badges.push("Hello, my name is #{attendees}.")
+def batch_badge_creator(attendees)
+  attendees.each |name| "Hello, my name is #{name}."
 end
